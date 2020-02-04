11752aaf744f493b035276042871f02b5c50ae37
Denys <denys.cheporniuk@gmail.com>
2020-02-04 16:21:26 +0200

    Updates

---
+++ precache-manifest.dd2cbdbb78ae1b31c6aada1fb7b93fc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e34e234b880b2be30dfa6ff89c717dba",
+    "url": "/YARANDIN_test-task/index.html"
+  },
+  {
+    "revision": "e55eed1212c8d79e3080",
+    "url": "/YARANDIN_test-task/static/css/main.5fe49172.chunk.css"
+  },
+  {
+    "revision": "fffcfa6ab8d917fa4933",
+    "url": "/YARANDIN_test-task/static/js/2.232898fc.chunk.js"
+  },
+  {
+    "revision": "e55eed1212c8d79e3080",
+    "url": "/YARANDIN_test-task/static/js/main.423187af.chunk.js"
+  },
+  {
+    "revision": "339361cff7b8c0b9beb8",
+    "url": "/YARANDIN_test-task/static/js/runtime~main.92f52329.js"
+  }
+]);
